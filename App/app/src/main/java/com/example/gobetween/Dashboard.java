59cec6c300cb4752fec5e42f2dc1c8e4d9fe101e
sree-jith-k-s <sreejithksreenivasan376@gmail.com>
2024-02-06 00:46:39 +0530

    searchcompany

--- App/app/src/main/java/com/example/gobetween/Dashboard.java
+++ App/app/src/main/java/com/example/gobetween/Dashboard.java
@@ -8,17 +8,25 @@ import android.os.Bundle;
 import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
-        CardView card1;
+        CardView card1,card2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         card1=findViewById(R.id.card1);
+        card2=findViewById(R.id.card2);
 
         card1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(Dashboard.this, Myprofile.class);
+                startActivity(i);
+            }
+        });
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Dashboard.this, Searchcompany.class);
                 startActivity(i);
             }
         });
